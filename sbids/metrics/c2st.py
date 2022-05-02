2dc608b8b4dae8b57adc40901310bbfea6af1999
Justine <zghjustinephd@gmail.com>
2022-05-02 12:07:22 +0200

    PR comments
    Change the classifier architecture, the loss, the seed,

--- sbids/metrics/c2st.py
+++ sbids/metrics/c2st.py
@@ -10,27 +10,23 @@ tfd = tfp.distributions
 tfb = tfp.bijectors
 
 class Classifier_C2ST(hk.Module):
+  """Binary classifier with 2 hidden layers of 10x dim each, 
+  following the architecture of the Benchmarking Simulation-Based Inference 
+  paper arXiv:2101.04653v2 """
 
-  def __call__(self, x):
+  def __call__(self, x, ndim):
 
-    net_x = hk.Linear(8)(x) 
+    net_x = hk.Linear(10 * ndim)(x) 
     net_x = jax.nn.leaky_relu(net_x)
-    net_x = hk.Linear(16)(net_x) 
-    net_x = jax.nn.leaky_relu(net_x) 
-    net_x = hk.Linear(32)(net_x) 
-    net_x = jax.nn.leaky_relu(net_x) 
-    net_x = hk.Linear(16)(net_x) 
-    net_x = jax.nn.leaky_relu(net_x) 
-    net_x = hk.Linear(8)(net_x) 
+    net_x = hk.Linear(10 * ndim)(net_x) 
     net_x = jax.nn.leaky_relu(net_x) 
     net_x = hk.Linear(1)(net_x) 
     net_x = jax.nn.sigmoid(net_x).squeeze()
-    net_x = jnp.clip(net_x, 1e-5, 1.-1e-5)
 
     return net_x
 
 
-def c2st(sample1,sample2,num_seed):
+def c2st(sample1,sample2,haikuseed):
   """
   Classifier Two-Sample Test
 
@@ -40,15 +36,14 @@ def c2st(sample1,sample2,num_seed):
   Parameters
    ----------
    sample1: First sample.
-   sample2 : Second sample.
-   num_seed : seed number
+   sample2: Second sample.
+   haikuseed: Haiku Iterator of JAX random keys. 
    Returns
    -------
-     Accuracy.
+     Loss and Accuracy.
    
   """
 
-  
   sample1_mean = jnp.mean(sample1, axis = 0)
   sample1_std = jnp.std(sample1, axis = 0)
   sample1 = (sample1 - sample1_mean) / sample1_std
@@ -56,18 +51,16 @@ def c2st(sample1,sample2,num_seed):
 
   label1 = jnp.zeros(len(sample1)) 
   label2 = jnp.ones(len(sample2))  
-  data = jnp.concatenate([jnp.concatenate([sample1,label1.reshape(-1,1)],axis = 1),
-                          jnp.concatenate([sample2,label2.reshape(-1,1)],axis = 1)],axis = 0)
+  data = jnp.concatenate([sample1,sample2],axis = 0)
+  label = jnp.concatenate([label1,label2],axis = 0)
 
-  data = jax.random.permutation(jax.random.PRNGKey(num_seed), data, axis=0)
+  inds = np.random.randint(0, len(data), len(data)) 
+  data, label = data[inds], label[inds]
   ntrain = int((len(sample1)+len(sample2))*0.6)
   dtrain, dte = data[:ntrain], data[ntrain:]
+  ltrain,lte = label[:ntrain], label[ntrain:]
 
   sample_dim = sample1.shape[1]
-  ztrain = dtrain[...,:sample_dim]
-  ltrain = dtrain[...,sample_dim:].squeeze()
-  zte = dte[...,:sample_dim]
-  lte = dte[...,sample_dim:].squeeze()
 
   batch_size = 560
   num_batches = ntrain // batch_size 
@@ -81,13 +74,13 @@ def c2st(sample1,sample2,num_seed):
         perm = rng.permutation(ntrain)
         for i in range(num_batches):
           batch_idx = perm[i * batch_size: (i + 1)*batch_size]
-          yield ztrain[batch_idx], ltrain[batch_idx]
+          yield dtrain[batch_idx], ltrain[batch_idx]
 
   batches = data_stream()
 
 
-  c2stClassifier = hk.without_apply_rng(hk.transform(lambda x : Classifier_C2ST()(x)))
-  rng_seq = hk.PRNGSequence(num_seed)
+  c2stClassifier = hk.without_apply_rng(hk.transform(lambda x : Classifier_C2ST()(x, sample_dim)))
+  rng_seq = haikuseed
   params_c2stClassifier = c2stClassifier.init(next(rng_seq), 0.5*jnp.ones(sample_dim))
   scheduler = optax.exponential_decay(init_value=0.001, transition_steps=2000, decay_rate=0.9, end_value=0.00001)
   optimizer_c2stClassifier = optax.chain(optax.scale_by_adam(), optax.scale_by_schedule(scheduler), optax.scale(-1))
@@ -97,9 +90,9 @@ def c2st(sample1,sample2,num_seed):
   @jax.jit
   def logloss(params,batch,label):
       """Loss"""
-      prob = jax.vmap(lambda x : c2stClassifier.apply(params,x.reshape(4)))(batch)
-      test = (1-label)*jnp.log(1-prob)
-      return -jnp.mean(label*jnp.log(prob)+(1-label)*jnp.log(1-prob))
+      logits = jax.vmap(lambda x : c2stClassifier.apply(params,x.reshape(4)))(batch)
+      return jnp.mean(optax.sigmoid_binary_cross_entropy(logits, label),axis=0)
+
 
   @jax.jit
   def update_c2stClassifier(params, opt_state, batch, label):
@@ -120,7 +113,7 @@ def c2st(sample1,sample2,num_seed):
     
 
   # compute metric 
-  prob = jax.vmap(lambda x : c2stClassifier.apply(params_c2stClassifier,x.reshape(sample_dim)))(zte)
+  prob = jax.vmap(lambda x : c2stClassifier.apply(params_c2stClassifier,x.reshape(sample_dim)))(dte)
   prob = prob.at[prob <= 0.5].set(0)
   prob = prob.at[prob > 0.5].set(1)
   t = jnp.mean(prob==lte)
