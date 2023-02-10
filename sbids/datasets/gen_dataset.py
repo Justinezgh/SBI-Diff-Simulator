91f2c108e552378b4a9a064e7bb86387840b4a2e
Justine <zghjustinephd@gmail.com>
2023-02-10 16:45:23 +0100

    delete useless files

--- sbids/datasets/gen_dataset.py
+++
@@ -1,4 +0,0 @@
-from utils import load_dataset
-
-train = load_dataset(name = 'LensingLogNormalDataset/year_1_score_density')
-train = load_dataset(name = 'LensingLogNormalDataset/year_10_score_density')
